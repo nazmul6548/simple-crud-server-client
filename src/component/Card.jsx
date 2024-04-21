99a4eca5d1301ca20ff032f682154a420b7821e8
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-04-21 17:33:17 +0600

    task

---
+++ src/component/Card.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+
+const Card = ({setUser,singleUser,user}) => {
+    
+    const {name,email,_id} = singleUser;
+    console.log(name);
+    const handleDelete = _id => {
+console.log("delete",_id);
+fetch(`http://localhost:5000/users/${_id}`,{
+    method: 'DELETE'
+})
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    if (data.deletedCount> 0) {
+        alert("you have deleted successfully")
+        const remaining = user.filter(user => user._id !== _id)
+                setUser(remaining);
+                
+    }
+})
+// .catch(error => {
+//     console.error('Error:', error);
+//     alert("Failed to delete user");
+// });
+    };
+    return (
+        <div>
+               <div className=" p-6 rounded-md shadow-md bg-gray-900 text-gray-50">
+	<img src="https://source.unsplash.com/random/300x300/?1" alt="" className="object-cover object-center w-full rounded-md h-72 bg-gray-500" />
+	<div className="mt-6 mb-2">
+		<span className="block text-xs font-medium tracking-widest uppercase text-violet-400">id: {_id}</span>
+		<h2 className="text-xl font-semibold tracking-wide">name : {name}</h2>
+	</div>
+	<p className="text-gray-100">email : {email}  </p>
+    <button onClick={() => handleDelete(_id)} className=" hover:bg-emerald-900 py-3 mt-2 w-full">Delete</button>
+</div>
+        </div>
+    );
+};
+
+export default Card;
--- src/component/Users.jsx
+++ src/component/Users.jsx
@@ -1,12 +1,20 @@
 import { useLoaderData } from "react-router-dom";
+import Card from "./Card";
+import { useState } from "react";
 
 
 const Users = () => {
     const users = useLoaderData()
+    const [user,setUser] = useState(users)
     console.log(users);
     return (
         <div>
             <h2>hi there is users component {users.length}</h2>
+         <div className="grid grid-cols-3 gap-10 mt-12">
+         {
+            user.map((singleUser) =><Card key={user._id} setUser={setUser} user={user} singleUser={singleUser}></Card>)
+         }
+         </div>
         </div>
     );
 };
